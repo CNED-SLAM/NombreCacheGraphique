25654f35c7d4adb8e07ba80ba271fe7a2f285fd2
CNED-SLAM <emds@bodhissatva.com>
2025-01-06 17:28:50 +0100

    Phase 1 : application fonctionnelle

---
+++ NombreCacheGraphique/FrmNombreCache.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace NombreCacheGraphique
+{
+    partial class FrmNombreCache
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValeur = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.grpValeur = new System.Windows.Forms.GroupBox();
+            this.grpReponse = new System.Windows.Forms.GroupBox();
+            this.btnRejouer = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.grpValeur.SuspendLayout();
+            this.grpReponse.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtValeur
+            // 
+            this.txtValeur.Location = new System.Drawing.Point(6, 19);
+            this.txtValeur.Name = "txtValeur";
+            this.txtValeur.Size = new System.Drawing.Size(108, 20);
+            this.txtValeur.TabIndex = 0;
+            this.txtValeur.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtValeur.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtValeur_KeyPress);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(6, 16);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(49, 13);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "message";
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(120, 19);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(36, 20);
+            this.btnValider.TabIndex = 3;
+            this.btnValider.Text = "OK";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.BtnValider_Click);
+            // 
+            // grpValeur
+            // 
+            this.grpValeur.Controls.Add(this.txtValeur);
+            this.grpValeur.Controls.Add(this.btnValider);
+            this.grpValeur.Location = new System.Drawing.Point(12, 12);
+            this.grpValeur.Name = "grpValeur";
+            this.grpValeur.Size = new System.Drawing.Size(168, 50);
+            this.grpValeur.TabIndex = 0;
+            this.grpValeur.TabStop = false;
+            // 
+            // grpReponse
+            // 
+            this.grpReponse.Controls.Add(this.lblMessage);
+            this.grpReponse.Location = new System.Drawing.Point(12, 68);
+            this.grpReponse.Name = "grpReponse";
+            this.grpReponse.Size = new System.Drawing.Size(168, 42);
+            this.grpReponse.TabIndex = 6;
+            this.grpReponse.TabStop = false;
+            // 
+            // btnRejouer
+            // 
+            this.btnRejouer.Image = global::NombreCacheGraphique.Properties.Resources.playagain;
+            this.btnRejouer.Location = new System.Drawing.Point(186, 12);
+            this.btnRejouer.Name = "btnRejouer";
+            this.btnRejouer.Size = new System.Drawing.Size(46, 46);
+            this.btnRejouer.TabIndex = 7;
+            this.btnRejouer.UseVisualStyleBackColor = true;
+            this.btnRejouer.Click += new System.EventHandler(this.BtnRejouer_Click);
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Image = global::NombreCacheGraphique.Properties.Resources.quitter;
+            this.btnQuitter.Location = new System.Drawing.Point(186, 64);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(46, 46);
+            this.btnQuitter.TabIndex = 4;
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.BtnQuitter_Click);
+            // 
+            // FrmNombreCache
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 120);
+            this.Controls.Add(this.btnRejouer);
+            this.Controls.Add(this.grpReponse);
+            this.Controls.Add(this.grpValeur);
+            this.Controls.Add(this.btnQuitter);
+            this.Name = "FrmNombreCache";
+            this.Text = "nombre caché";
+            this.Load += new System.EventHandler(this.FrmNombreCache_Load);
+            this.grpValeur.ResumeLayout(false);
+            this.grpValeur.PerformLayout();
+            this.grpReponse.ResumeLayout(false);
+            this.grpReponse.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtValeur;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.GroupBox grpValeur;
+        private System.Windows.Forms.GroupBox grpReponse;
+        private System.Windows.Forms.Button btnRejouer;
+    }
+}
+
---
+++ NombreCacheGraphique/FrmNombreCache.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NombreCacheGraphique
+{
+    public partial class FrmNombreCache : Form
+    {
+        // déclaration globale
+        private int phase;  // phase 1 : saisie du nombre à chercher ; pahase 2 : recherche
+        private int valeurAChercher; // contiendra la valeur à chercher
+        private int nbEssai; // nombre d'essais pour trouver la valeur
+
+        /// <summary>
+        /// initialisation des composants graphiques
+        /// </summary>
+        public FrmNombreCache()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Chargement de la fenêtre : initialisations pour commencer le jeu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmNombreCache_Load(object sender, EventArgs e)
+        {
+            BtnRejouer_Click(null, null);
+        }
+
+        /// <summary>
+        /// Réinitialisations au début du jeu ou au début des tentatives
+        /// </summary>
+        private void Initialiser()
+        {
+            if (phase == 1)
+            {
+                grpValeur.Text = "Valeur (entre 1 et 100)";
+                grpValeur.Enabled = true;
+                grpReponse.Visible = false;
+            }
+            else
+            {
+                grpValeur.Text = "Essai  (entre 1 et 100)";
+                nbEssai = 0;
+                grpReponse.Text = "";
+                grpReponse.Visible = true;
+            }
+            lblMessage.Text = "";
+            EffacerZoneSaisie();
+        }
+
+        /// <summary>
+        /// Clic sur le bouton btnRejouer (flèche ronde) : démarrage d'un nouveau jeu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnRejouer_Click(object sender, EventArgs e)
+        {
+            phase = 1;
+            Initialiser();
+        }
+
+        /// <summary>
+        /// Clic sur le bouton btnQuitter : fermeture de l'application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Clic sur le bouton btnValider (OK)
+        /// Si 1ère phase de jeu : contrôle le nombre(entre 1 et 100) et lance la 2ème phase(recherche)
+        /// Si 2ème phase de jeu : compare l'essai avec le nombre de départ et affiche le message
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnValider_Click(object sender, EventArgs e)
+        {
+            //  contrôle si la valeur saisie est bien un entier
+            try
+            {
+                int valeur = int.Parse(txtValeur.Text);
+                // contrôle si le nombre est entre 1 et 100
+                if (valeur < 1 || valeur > 100)
+                {
+                    EffacerZoneSaisie();
+                }
+                else
+                {
+                    if (phase == 1)
+                    {
+                        // mémorisation de la valeur à chercher
+                        valeurAChercher = valeur;
+                        // passage à la phase 2
+                        phase = 2;
+                        Initialiser();
+                    }
+                    else
+                    {
+                        // affiche le nombre d'essais
+                        nbEssai++;
+                        grpReponse.Text = "Essai n°" + nbEssai;
+                        // comparaison et affichage du message correspondant
+                        if (valeur == valeurAChercher)
+                        {
+                            lblMessage.Text = "Bravo !!! C'était bien " + valeurAChercher;
+                            EffacerZoneSaisie();
+                            grpValeur.Enabled = false;
+                            btnRejouer.Focus();
+                        }
+                        else
+                        {
+                            if (valeur < valeurAChercher)
+                            {
+                                lblMessage.Text = valeur + " est trop petit";
+                            }
+                            else
+                            {
+                                lblMessage.Text = valeur + " est trop grand";
+                            }
+                            EffacerZoneSaisie();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                EffacerZoneSaisie();
+            }
+        }
+
+        /// <summary>
+        /// Efface la zone de saisie et repositionne le curseur
+        /// </summary>
+        private void EffacerZoneSaisie()
+        {
+            txtValeur.Text = "";
+            txtValeur.Focus();
+        }
+
+        /// <summary>
+        /// Validation dans txtValeur
+        /// Même effet que le clic sur le bouton ok
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TxtValeur_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                BtnValider_Click(null, null);
+            }
+        }
+    }
+}
